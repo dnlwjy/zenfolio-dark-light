1bd6dd75f056c8adea95489d6e87c7db3444d1f8
Daniel Wijaya <wijayadaniel19@gmail.com>
2026-06-10 14:02:22 +0700

    added new assets

--- lib/isSafariBrowser.ts
+++
@@ -1,5 +0,0 @@
-export function isSafariBrowser(): boolean {
-    if (typeof navigator === "undefined") return false
-    const ua = navigator.userAgent
-    return /Safari/i.test(ua) && !/Chrome|Chromium|CriOS|Edg|OPR|Android|FxiOS/i.test(ua)
-}
